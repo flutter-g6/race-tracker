cff0ec1f7ea0b2a1c28395a554af7121ecd1b6cc
unknown <sinphourivath@gmail.com>
2025-04-22 08:33:22 +0700

    RT-8 add test for participant registraion form

---
+++ lib/ui/screens/rt_form.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class RTForm extends StatelessWidget {
+  const RTForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
