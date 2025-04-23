5be6a59fa8e030c979d2cc8958ff669c60da7492
Seang Sokheng <wwhengww@gmail.com>
2025-04-23 11:25:52 +0700

    add model,dto,repository mock and firebase

---
+++ lib/data/repository/mock/mock_participant_repository.dart
@@ -0,0 +1,31 @@
+import 'package:race_tracker/model/participant.dart';
+import 'package:race_tracker/data/repository/participant_repository.dart';
+
+class FakeParticipantRepository extends ParticipantRepository {
+  final List<Participant> _participants = [];
+
+  @override
+  Future<List<Participant>> getParticipants() async {
+    await Future.delayed(Duration(milliseconds: 500));
+    return _participants;
+  }
+
+  @override
+  Future<void> addParticipant(Participant participant) async {
+    await Future.delayed(Duration(milliseconds: 500));
+    _participants.add(participant);
+  }
+
+  @override
+  Future<void> updateParticipant(Participant participant) async {
+    await Future.delayed(Duration(milliseconds: 500));
+    final index = _participants.indexWhere((p) => p.id == participant.id);
+    if (index != -1) _participants[index] = participant;
+  }
+
+  @override
+  Future<void> deleteParticipant(String id) async {
+    await Future.delayed(Duration(milliseconds: 500));
+    _participants.removeWhere((p) => p.id == id);
+  }
+}
---
+++ lib/data/repository/participant_repository.dart
@@ -0,0 +1,8 @@
+import 'package:race_tracker/model/participant.dart';
+
+abstract class ParticipantRepository {
+  Future<List<Participant>> getParticipants();
+  Future<void> addParticipant(Participant participant);
+  Future<void> updateParticipant(Participant participant);
+  Future<void> deleteParticipant(String id);
+}
