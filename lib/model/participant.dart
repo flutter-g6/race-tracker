c3d3cf7d7e8fe10d8939a7c49ba16d5f2b3f7d54
Seang Sokheng <wwhengww@gmail.com>
2025-04-23 13:02:41 +0700

    RT-15 Implement repository pattern

--- lib/model/participant.dart
+++ lib/model/participant.dart
@@ -1,6 +1,35 @@
-// TODO: add participant field
 class Participant {
-  Participant({required this.id});
+  final String id;
+  final String bib;
+  final String firstName;
+  final String lastName;
+  final int age;
+  final String gender;
 
-  String id;
+  Participant({
+    required this.id,
+    required this.bib,
+    required this.firstName,
+    required this.lastName,
+    required this.age,
+    required this.gender,
+  });
+
+  Participant copyWith({
+    String? id,
+    String? bib,
+    String? firstName,
+    String? lastName,
+    int? age,
+    String? gender,
+  }) {
+    return Participant(
+      id: id ?? this.id,
+      bib: bib ?? this.bib,
+      firstName: firstName ?? this.firstName,
+      lastName: lastName ?? this.lastName,
+      age: age ?? this.age,
+      gender: gender ?? this.gender,
+    );
+  }
 }
