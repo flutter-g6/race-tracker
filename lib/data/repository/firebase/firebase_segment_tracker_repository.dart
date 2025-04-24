7361e0e3c6c56c94de12274324773d055b0151cc
Seang Sokheng <wwhengww@gmail.com>
2025-04-24 16:55:19 +0700

    RT-30 Start all participant on for specific segment

--- lib/data/repository/firebase/firebase_segment_tracker_repository.dart
+++ lib/data/repository/firebase/firebase_segment_tracker_repository.dart
@@ -1,12 +1,15 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:race_tracker/data/dto/segment_record.dart';
+import 'package:race_tracker/data/repository/firebase/firebase_participant_repository.dart';
 import 'package:race_tracker/data/repository/firebase/firebase_race_repository.dart';
 import 'package:race_tracker/data/repository/segment_tracker_repository.dart';
+import 'package:race_tracker/model/participant.dart';
 import 'package:race_tracker/model/race.dart';
 import 'package:race_tracker/model/segment_record.dart';
 
 class FirebaseSegmentTrackerRepository extends SegmentTrackerRepository {
   final FirebaseRaceRepository _raceRepository = FirebaseRaceRepository();
+  final FirebaseParticipantRepository _participantRepository = FirebaseParticipantRepository();
   final _db = FirebaseDatabase.instance.ref();
 
   Future<bool> _isRaceOngoing(String raceId) async {
@@ -35,5 +38,26 @@ class FirebaseSegmentTrackerRepository extends SegmentTrackerRepository {
     final finishTime = DateTime.now().toIso8601String();
     final finishRef = _db.child('race_segments/$raceId/${segment.name}/$bib/finishTime');
     await finishRef.set(finishTime);
+  }
+
+  @override
+  Future<void> startAllParticipantsForSegment(String raceId, Segment segment) async {
+    if (!await _isRaceOngoing(raceId)) {
+      throw Exception("Race is not ongoing");
+    }
+
+    final List<Participant> participants = await _participantRepository.getParticipants();
+
+    final segmentRef = _db.child('race_segments/$raceId/${segment.name}');
+
+    for (final participant in participants) {
+      final record = SegmentRecord(
+        bib: participant.bib,
+        segment: segment,
+        startTime: DateTime.now(),
+      );
+
+      await segmentRef.child(participant.bib).set(SegmentRecordDto.toJson(record));
+    }
   }
 }
--- lib/data/repository/segment_tracker_repository.dart
+++ lib/data/repository/segment_tracker_repository.dart
@@ -3,6 +3,7 @@ import 'package:race_tracker/model/segment_record.dart';
 abstract class SegmentTrackerRepository {
   Future<void> startSegment(String raceId, String bib, Segment segment);
   Future<void> finishSegment(String raceId, String bib, Segment  segment);
+  Future<void> startAllParticipantsForSegment (String raceId, Segment segment);
   //Stream<SegmentRecord?> watchSegment(String raceId, String bib, String segment);
 }
 
